2afaf0bd7ba2a3e092012b4fef0d7e82e49849e8
parthmerchant <parthmmerchant@gmail.com>
2021-02-26 13:44:53 -0500

    card and color picker

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
+import { ChromePicker, HuePicker, TwitterPicker } from 'react-color';
 import "./Cards.css";
 
 class Cards extends Component{
 
-    componentDidMount() {
-        window.scrollTo(0, 0)
+    state = {
+        background: '#fff',
+    };
+
+    handleChange = (color) => {
+        this.setState({ background: color.hex });
     }
+    
+    handleChangeComplete = (color) => {
+        this.setState({ background: color.hex });
+    };
     
     render(){
         return(
             <div className="cards-container">
-                <h1>Welcome to Cards</h1>
+                <div class="row">
+                    <div class="column">
+                        <div className="main-card" style={{backgroundColor: this.state.background }}>
+                            <p>Cards are cool!</p>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="color-controller">
+                            <ChromePicker color={ this.state.background } onChange={ this.handleChange } onChangeComplete={ this.handleChangeComplete } />
+                            <br></br>
+                            <HuePicker color={ this.state.background } onChange={ this.handleChange } onChangeComplete={ this.handleChangeComplete } />
+                            <br></br>
+                            <TwitterPicker color={ this.state.background } onChange={ this.handleChange } onChangeComplete={ this.handleChangeComplete } />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
